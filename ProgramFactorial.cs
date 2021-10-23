5d3ae28c593ad2b38114a6e79eaaa11c852b026b
TypicalIsekaidGuy <91285375+TypicalIsekaidGuy@users.noreply.github.com>
2021-10-23 07:10:33 +0300

    Add files via upload

---
+++ ProgramFactorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = Convert.ToInt32(Console.ReadLine());
+            int sum = 1;
+            int i = 1;
+            do
+            {
+                i++;
+                sum *= i;
+
+            } while (i < p);
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
