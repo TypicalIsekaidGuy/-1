0accc81572c8bddd18b8a7bbc8f5f9260454f30c
TypicalIsekaidGuy <91285375+TypicalIsekaidGuy@users.noreply.github.com>
2021-10-23 07:08:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int sum = 0;
+            N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < N; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"Vashe chislo: {sum / N}");
+        }
+    }
+}
