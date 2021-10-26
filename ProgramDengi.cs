b7834498664284e893f68185f4b8b15f49e2a379
TypicalIsekaidGuy <91285375+TypicalIsekaidGuy@users.noreply.github.com>
2021-10-26 11:54:01 +0300

    Add files via upload

---
+++ ProgramDengi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите на сколько клиент скупил товара:");
+            int roflan = Convert.ToInt32(Console.ReadLine());
+            switch (roflan)
+            {
+                case > 500 when roflan < 1000:
+                    Console.WriteLine($"Всего покупатель заплатит {roflan * 0.97}");
+                    break;
+                case > 1000:
+                    Console.WriteLine($"Всего покупатель заплатит {roflan * 0.95}");
+                    break;
+                default:
+                    Console.WriteLine($"Всего покупатель заплатит {roflan}");
+                    break;
+            }
+        }
+    }
+}
