ffb42a307e2072f0e49c4963e8c486f092c7d98b
TypicalIsekaidGuy <91285375+TypicalIsekaidGuy@users.noreply.github.com>
2021-11-19 20:17:02 +0300

    Add files via upload

---
+++ Program3Nubmers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+            if (x > 0 && x < 4)
+            {
+                Console.WriteLine("x входит");
+            }
+            else
+            {
+
+                Console.WriteLine("x не входит");
+            }
+            if (y > 0 && y < 4)
+            {
+                Console.WriteLine("y входит");
+            }
+            else
+            {
+
+                Console.WriteLine("y не входит");
+            }
+            if (z > 0 && z < 4)
+            {
+                Console.WriteLine("z входит");
+            }
+            else
+            {
+
+                Console.WriteLine("z не входит");
+            }
+        }
+    }
+}
