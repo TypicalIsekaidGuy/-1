77e74112633d3d76d7b86ac219ef93e35503328f
TypicalIsekaidGuy <91285375+TypicalIsekaidGuy@users.noreply.github.com>
2021-11-20 14:35:29 +0300

    Add files via upload

---
+++ ProgramPolyndrom.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class HelloWorld
+{
+    static void main(string[] args)
+    {
+        int snus = 0;
+        string cock = Convert.ToString(Console.ReadLine());
+        string aue ="";
+        for (int i = 0; i < (cock.Length); i++)
+        {
+            if (i != 0)
+            {
+                aue += cock[i];
+            }
+            else
+            {
+                aue += cock[i];
+                aue.ToUpper();
+            }
+
+        }
+        Console.WriteLine($"{aue}");
+    }
+
+}
