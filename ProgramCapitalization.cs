d68d79c05f441ced41725bab143939bfff112ce9
TypicalIsekaidGuy <91285375+TypicalIsekaidGuy@users.noreply.github.com>
2021-11-20 14:35:18 +0300

    Add files via upload

---
+++ ProgramCapitalization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ConsoleApp7
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            int snus = 0;
+            string cock = Convert.ToString(Console.ReadLine());
+            string aue = "";
+            for (int i = 0; i < (cock.Length); i++)
+            {
+                if (i != 0)
+                {
+                    aue += cock[i];
+                }
+                else
+                {
+                    aue += cock[i];
+                    aue = aue.ToUpper();
+                }
+
+            }
+            Console.WriteLine($"{aue}");
+        }
+
+    }
+}
