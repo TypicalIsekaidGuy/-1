16ce1c7137b30a71ad86ff0ba14712478c020fc0
TypicalIsekaidGuy <91285375+TypicalIsekaidGuy@users.noreply.github.com>
2021-12-02 12:20:55 +0300

    Add files via upload

---
+++ ProgramPerimetr1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+        }
+        static void Perimetr(int a,int b,int c)
+
+        {
+            Console.WriteLine($"{a+b+c}");
+        }
+    }
+}
